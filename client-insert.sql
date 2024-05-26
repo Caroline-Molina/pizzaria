3e0d0e591d859771aa6772da0d934d134994477e
Caroline-Molina <carolinebstmolina@gmail.com>
2024-05-26 15:06:26 -0300

    Insert Registros - Cliente

---
+++ client-insert.sql
@@ -0,0 +1,13 @@
+use pizzariaDB;
+
+-- Tabela Cliente
+
+insert into Cliente (nomeCompleto, nome, sobrenome, endereco, tipoCliente) values ("Gregório de Matos","Gregorio","Matos","Rua A","iFood");
+insert into Cliente (nomeCompleto, nome, sobrenome, endereco, tipoCliente) values ("Gonçalves Dias","Gonçalves","Dias","Rua B","Rappi");
+insert into Cliente (nomeCompleto, nome, sobrenome, endereco, tipoCliente) values ("Álvares de Azevedo","Álvares","Azevedo","Rua C","Balcão");
+insert into Cliente (nomeCompleto, nome, sobrenome, endereco, tipoCliente) values ("Castro Alves","Castro","Alves","Rua D","iFood");
+insert into Cliente (nomeCompleto, nome, sobrenome, endereco, tipoCliente) values ("José de Alencar","José","Alencar","Rua E","Rappi");
+insert into Cliente (nomeCompleto, nome, sobrenome, endereco, tipoCliente) values ("Bernardo Guimarães","Bernardo","Guimarães","Rua F","Balcão");
+insert into Cliente (nomeCompleto, nome, sobrenome, endereco, tipoCliente) values ("Machado de Assis","Machado","Assis","Rua G","iFood");
+insert into Cliente (nomeCompleto, nome, sobrenome, endereco, tipoCliente) values ("Aluísio Azevedo","Aluísio","Azevedo","Rua H","Rappi");
+
