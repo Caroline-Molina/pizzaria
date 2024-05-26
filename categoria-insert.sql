40280b652440a53297e3534c2f17d9593ad23fe5
Caroline-Molina <carolinebstmolina@gmail.com>
2024-05-26 15:00:44 -0300

    Insert Registros - Categoria

---
+++ categoria-insert.sql
@@ -0,0 +1,23 @@
+use pizzariaDB;
+
+-- Tabela Categoria
+
+insert into Categoria (nomeCategoria,preco) values ("Especial_I",100);
+insert into Categoria (nomeCategoria,preco) values ("Especial_II",110);
+insert into Categoria (nomeCategoria,preco) values ("Tradicional_I",60);
+insert into Categoria (nomeCategoria,preco) values ("Tradicional_II",65);
+insert into Categoria (nomeCategoria,preco) values ("Tradicional_III",70);
+insert into Categoria (nomeCategoria,preco) values ("Tradicional_IV",80);
+insert into Categoria (nomeCategoria,preco) values ("Gourmet_I",90);
+insert into Categoria (nomeCategoria,preco) values ("Gourmet_II",95);
+insert into Categoria (nomeCategoria,preco) values ("Doces",70);
+insert into Categoria (nomeCategoria,preco) values ("Vegana",80);
+insert into Categoria (nomeCategoria,preco) values ("Promocao_I",42.99);
+insert into Categoria (nomeCategoria,preco) values ("Promocao_II",43.99);
+insert into Categoria (nomeCategoria,preco) values ("Promocao_III",47.99);
+insert into Categoria (nomeCategoria,preco) values ("Promocao_IV",48.99);
+
+
+
+
+
