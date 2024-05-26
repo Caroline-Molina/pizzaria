7462ba5873b93c50e32ebfabb922e9c291e5b568
Caroline-Molina <carolinebstmolina@gmail.com>
2024-05-26 15:04:46 -0300

    Insert Registros - Pizza

---
+++ pizza-insert.sql
@@ -0,0 +1,22 @@
+use pizzariaDB;
+
+-- Tabela Pizza
+
+insert into Pizza (nomePizza, descricao, nomeCategoria) values ("Abobrinha","Mussarela de búfala, abobrinha em rodelas","Especial_I");
+insert into Pizza (nomePizza, descricao, nomeCategoria) values ("Alcachofra","Alcachofra, alcaparras, mussarela ou catupiry","Especial_II");
+insert into Pizza (nomePizza, descricao, nomeCategoria) values ("Mussarela","Queijo mussarela fatiado","Tradicional_I");
+insert into Pizza (nomePizza, descricao, nomeCategoria) values ("Calabresa","Calabresa fatiada e cebola","Tradicional_I");
+insert into Pizza (nomePizza, descricao, nomeCategoria) values ("Baiana","Calabresa moida, rodelas de tomate e parmesão","Tradicional_II");
+insert into Pizza (nomePizza, descricao, nomeCategoria) values ("Frango com Catupiry","Frando desfiado e catupiry","Tradicional_III");
+insert into Pizza (nomePizza, descricao, nomeCategoria) values ("Quatro Queijos","Mussarela, catupiry, provolone e parmesão","Tradicional_IV");
+insert into Pizza (nomePizza, descricao, nomeCategoria) values ("Italiana","Mussarela de búfala, rúcula e presunto parma","Gourmet_I");
+insert into Pizza (nomePizza, descricao, nomeCategoria) values ("Rucula","Mussarela de búfala, tomates secos e rucula","Gourmet_II");
+insert into Pizza (nomePizza, descricao, nomeCategoria) values ("Chocolate","Chocolate preto ou branco, cereja e granulado","Doces");
+insert into Pizza (nomePizza, descricao, nomeCategoria) values ("Vegana Bella","Berinjela, palmito, cebola e VeganChesse","Vegana");
+insert into Pizza (nomePizza, descricao, nomeCategoria) values ("Calabresa Especial","Molho de tomate fresco, fatias de calabresa com cebola, , ","Promocao_I");
+insert into Pizza (nomePizza, descricao, nomeCategoria) values ("Atum","Molho de tomate fresco, atum, rodelas de tomate e cebola","Promocao_II");
+insert into Pizza (nomePizza, descricao, nomeCategoria) values ("Carne Seca","Molho de tomate fresco, carne seca dessalgada e temperada com mussarela","Promocao_III");
+insert into Pizza (nomePizza, descricao, nomeCategoria) values ("Pepperoni","Molho de tomate fresco, mozarela e pepperoni fatiado","Promocao_IV");
+
+
+
